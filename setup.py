535423eda28569ff36ee28e7d4cb7f2fd9cb8a12
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2021-01-18 12:41:41 +0100

    Add Python 3.9 support and update PyPI classifiers

--- setup.py
+++ setup.py
@@ -20,10 +20,12 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Text Processing",
     ],
 )
